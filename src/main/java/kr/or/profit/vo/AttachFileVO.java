0da6332fb7377ccb0b0736b86417962998d0a2cf
liaminjeong <78000405+liaminjeong@users.noreply.github.com>
2021-06-24 20:17:18 +0900

    민정

---
+++ src/main/java/kr/or/profit/vo/AttachFileVO.java
@@ -0,0 +1,99 @@
+package kr.or.profit.vo;
+
+public class AttachFileVO {
+	private String fileSeq;
+	private String communitySeq;
+	private String memberId;
+	private String lessonDetailSeq;
+	private String chattingDetailSeq;
+	private String processSeq;
+	private String fileRealName;
+	private String fileSaveName;
+	private String filePath;
+	private String inUserId;
+	private String inDate;
+	private String upUserId;
+	private String upDate;
+	
+	
+	public String getFileSeq() {
+		return fileSeq;
+	}
+	public void setFileSeq(String fileSeq) {
+		this.fileSeq = fileSeq;
+	}
+	public String getCommunitySeq() {
+		return communitySeq;
+	}
+	public void setCommunitySeq(String communitySeq) {
+		this.communitySeq = communitySeq;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getLessonDetailSeq() {
+		return lessonDetailSeq;
+	}
+	public void setLessonDetailSeq(String lessonDetailSeq) {
+		this.lessonDetailSeq = lessonDetailSeq;
+	}
+	public String getChattingDetailSeq() {
+		return chattingDetailSeq;
+	}
+	public void setChattingDetailSeq(String chattingDetailSeq) {
+		this.chattingDetailSeq = chattingDetailSeq;
+	}
+	public String getProcessSeq() {
+		return processSeq;
+	}
+	public void setProcessSeq(String processSeq) {
+		this.processSeq = processSeq;
+	}
+	public String getFileRealName() {
+		return fileRealName;
+	}
+	public void setFileRealName(String fileRealName) {
+		this.fileRealName = fileRealName;
+	}
+	public String getFileSaveName() {
+		return fileSaveName;
+	}
+	public void setFileSaveName(String fileSaveName) {
+		this.fileSaveName = fileSaveName;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getInUserId() {
+		return inUserId;
+	}
+	public void setInUserId(String inUserId) {
+		this.inUserId = inUserId;
+	}
+	public String getInDate() {
+		return inDate;
+	}
+	public void setInDate(String inDate) {
+		this.inDate = inDate;
+	}
+	public String getUpUserId() {
+		return upUserId;
+	}
+	public void setUpUserId(String upUserId) {
+		this.upUserId = upUserId;
+	}
+	public String getUpDate() {
+		return upDate;
+	}
+	public void setUpDate(String upDate) {
+		this.upDate = upDate;
+	}
+	
+	
+}
