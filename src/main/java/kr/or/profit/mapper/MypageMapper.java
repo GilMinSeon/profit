325b7ca79670acf9efa02ec43d8121f070acd0e3
dpwls64 <52019440+dpwls64@users.noreply.github.com>
2021-06-25 14:59:25 +0900

    예진

--- src/main/java/kr/or/profit/mapper/MypageMapper.java
+++ src/main/java/kr/or/profit/mapper/MypageMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import kr.or.profit.vo.ProcessVO;
 
 //Mapper Interface => 메서드명과 쿼리ID를 매핑하여 쿼리호출
 
@@ -11,4 +12,7 @@ import egovframework.rte.psl.dataaccess.mapper.Mapper;
 public interface MypageMapper {
 	//회원기본정보조회
 	public List<Map<String, String>> selectMemberInfo(String memberId);
+	
+	//프로세스정보추가
+	public int insertProcess(ProcessVO vo);
 }
