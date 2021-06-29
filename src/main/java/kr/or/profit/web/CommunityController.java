2e028fe5e3b97543492615ae95545c68f64fc819
dpwls64 <52019440+dpwls64@users.noreply.github.com>
2021-06-29 10:09:30 +0900

    예진

--- src/main/java/kr/or/profit/web/CommunityController.java
+++ src/main/java/kr/or/profit/web/CommunityController.java
@@ -1,13 +1,29 @@
 package kr.or.profit.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.Locale;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.gson.JsonObject;
+
 
 /**
  * 
@@ -57,7 +73,7 @@ public class CommunityController {
 		return "community/boardDetail";
 	}
 	
-	@RequestMapping(value = "boardAdd", method = RequestMethod.GET)
+	@RequestMapping(value = "boardAdd.do", method = RequestMethod.GET)
 	public String boardAdd(Locale locale, Model model) {
 		
 		return "community/boardAdd";
@@ -68,6 +84,41 @@ public class CommunityController {
 		
 		return "community/boardMod";
 	}
+	
+//	@RequestMapping(value = "uploadSummernoteImageFileAjax.do", method = RequestMethod.GET)
+//	public String uploadSummernoteImageFileAjax(Locale locale, Model model) {
+//		System.out.println("제발");
+//		return "community/boardAdd";
+//	}
+	
+	@RequestMapping(value="profileImage.do",  method=RequestMethod.POST)
+	@ResponseBody
+	public void profileUpload(String folderName, MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	response.setContentType("text/html;charset=utf-8");
+	System.out.println("folderName : " + folderName );
+	System.out.println("휴...");
+	PrintWriter out = response.getWriter();
+	// 업로드할 폴더 경로
+	String realFolder = request.getSession().getServletContext().getRealPath("profileUpload");
+	UUID uuid = UUID.randomUUID();
 
+	// 업로드할 파일 이름
+	String org_filename = file.getOriginalFilename();
+	String str_filename = uuid.toString() + org_filename;
+
+	System.out.println("원본 파일명 : " + org_filename);
+	System.out.println("저장할 파일명 : " + str_filename);
+
+	String filepath = realFolder + "\\" + folderName + "\\" + str_filename;
+	System.out.println("파일경로 : " + filepath);
+
+	File f = new File(filepath);
+	if (!f.exists()) {
+	f.mkdirs();
+	}
+	file.transferTo(f);
+	out.println("profileUpload/"+folderName+"/"+str_filename);
+	out.close();
+	}
 	
 }
